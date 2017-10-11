1fa19a602f0f9780e431d6aabfa2fd51511a2c5a
Iman Alfathan Yudhanto <iman.alfathan@ui.ac.id>
2017-10-11 15:46:09 +0700

    First Commit tutorial 6. Masih butuh perbaikan..

---
+++ src/main/java/com/example/service/CourseService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+public interface CourseService {
+	CourseModel selectCourse(String idCourse);
+
+}
